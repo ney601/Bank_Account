0970af519a44178a8bfe8b9f0394033b0e2f8203
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-02-07 17:14:31 +0000

    Completed writing setters and getters for all properties.

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,2 +1,89 @@
+import java.time.LocalDate;
+
 public class BankAccount {
+
+    //Properties
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String accountNumber;
+    private int balance;
+    private String accountType;
+    private int overdraft;
+
+    // CONSTRUCTOR
+    public BankAccount(String firstName, String lastName, LocalDate dateOfBirth, String accountNumber, int balance, String accountType, int overdraft){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+        this.accountType = accountType;
+        this.overdraft = overdraft;
+    }
+// GETTERS & SETTERS
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public int getOverdraft() {
+        return overdraft;
+    }
+
+    public void setOverdraft(int overdraft) {
+        this.overdraft = overdraft;
+    }
+
+    // Methods
+    public void deposit(int amount){
+        balance += amount;
+    }
+
+    public void withdrawal(int amount) {
+
+    }
 }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,3 +1,15 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class BankAccountTest {
 
+    @Test
+    public void getDeposit(){
+        assertThat()
+    }
+
+
+    @BeforeAll
 }
